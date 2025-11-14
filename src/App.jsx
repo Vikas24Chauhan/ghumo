4cc5b65d9aad1079c7acae0b52c1ec1c23573803
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-11-14 11:19:35 +0530

    about us page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/HomePage";
-import PlacePages from "./pages/PlacePages";
+import Places from "./components/places/Places";
 import PlaceDetails from "./components/placeDetails/PlaceDetails";
+import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/places" element={<PlacePages />} />
+        <Route path="/places" element={<Places />} />
         <Route path="/place-details" element={<PlaceDetails />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </BrowserRouter>
